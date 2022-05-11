7764837a8965f9778edbd8cf417d427f4a61808b
Rafiee <rafiee300@gmail.com>
2022-05-10 22:31:16 -0700

    new class added

---
+++ src/Excercise.java
@@ -0,0 +1,16 @@
+
+public class Excercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		/**
+		 * this is a new class made into the local repo 
+		 * 
+		 */
+	}
+
+}
